16b68aa17be35a8adae5ed357f8719195c7b43a4
daksh-aghera-01 <dakshaghera111@gmail.com>
2026-02-17 16:41:56 +0530

    server update

--- server.js
+++ server.js
@@ -8,37 +8,48 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
 });
 
-io.on("connection", (socket) => {
-  console.log(`${socket.id} user just connected`);
 
-    //Join Room
-    socket.on("join_room", ({myId, otherUserId}) => {
+io.on("connection", (socket) => {
+    console.log(`${socket.id} user just connected`);
+
+
+    // Join Room
+    socket.on("join_room", ({ myId, otherUserId }) => {
         const roomName = [myId, otherUserId].sort().join("-");
 
-    socket.join(roomName);
+        socket.join(roomName);
 
-    console.log(`User ${myId} joined room: ${roomName}`);
+        console.log(`User ${myId} joined room: ${roomName}`);
 
-    socket.emit("room_joined", { roomId: roomName });
-  });
+        socket.emit("room_joined", { roomId: roomName });
+    });
 
-  //Send Message
-  socket.on("send_message", (data) => {
-      const {roomId, message, senderId, senderName } = data;
+    // Send Message
+    socket.on("send_message", (data) => {
+        const { roomId, message, senderId, senderName } = data;
+
+        console.log(`Message in ${roomId}: ${message}`);
+        
+        io.to(roomId).emit("receive_message", {
+            senderId: senderId,
+            senderName: senderName,
+            message: message,
+            timestamp: new Date().toISOString(),
+        });
+    });
 
     socket.on("disconnect", () => {
-        console.log(`User Dissconnected: ${socket.id}`);
-    })
-})
-})
+        console.log(`User Disconnected: ${socket.id}`);
+    });
+});
+
 const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server Running on Port ${PORT}`);
-    
-})
+});
